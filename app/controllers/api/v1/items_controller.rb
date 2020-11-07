db48b49166cdff53b363ff70749250ed032dc34a
jessicasalbert <jessicarsalbert@gmail.com>
2020-11-07 12:30:58 -0500

    jwt

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
-    def create
+    skip_before_action :authorized
+    def index
+        plants = Item.all
+        render json: plants, include: :price, except: [:updated_at, :created_at]
     end
 end

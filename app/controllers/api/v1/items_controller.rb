83db33ee8748ed1bb145d658f15b1e715235d620
gabrielhicks <gabrielhicks@mail.com>
2020-11-09 11:40:44 -0500

    newplant working

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::ItemsController < ApplicationController
     skip_before_action :authorized
+
     def index
         plants = Item.all
         render json: plants, include: :price, except: [:updated_at, :created_at]
     end
 
     def create
-        plant = Item.create(item_params)
+        item = Item.create(item_params)
 
-        render json: plant
+        render json: item
     end
 
     private

7917edf280fd25448a34aae80145a7227ecf2edd
jessicasalbert <jessicarsalbert@gmail.com>
2020-11-09 21:43:44 -0500

    render item details in profile

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,19 +1,14 @@
 class OrderSerializer < ApplicationSerializer
   attributes :id, :total, :created_at, :order_number, :purchases
-  # has_one :user
-  # has_many :purchases
-  # 
-
-  # def purchases
-  #   self.object.purchases.map do |pur|
-  #     {quantity: pur.quantity, size: pur.size}
-  #   end
-  # end
+ 
 
   def purchases
     ActiveModel::SerializableResource.new(object.purchases,  each_serializer: PurchaseSerializer)
   end
 
+  def created_at
+    self.object.created_at.in_time_zone("Eastern Time (US & Canada)").to_date
+  end
   
 
   
--- app/serializers/purchase_serializer.rb
+++ app/serializers/purchase_serializer.rb
@@ -1,19 +1,9 @@
 class PurchaseSerializer < ApplicationSerializer
   attributes :id, :quantity, :size, :price, :item_name
-  # has_one :order
-  # has_one :item
-  # accepts_nested_attributes_for :item
-
-  # def item
-  #   ActiveModel::SerializableResource.new(object.orders, each_serializer: OrderSerializer)
-  # end
 
   def item_name
     self.object.item.name
   end
 
-  # self.object.orders.map do |order|
-  #   #     {order_number: order.order_number, created_at: order.created_at, total: order.total}
-  #   # end
-
+ 
 end

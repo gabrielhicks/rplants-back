57354d72649fb0afcb269afba7f87b8d23d46a6d
jessicasalbert <jessicarsalbert@gmail.com>
2020-11-09 23:50:48 -0500

    guest checkout function

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-    skip_before_action :authorized, only: [:create, :show]
+    skip_before_action :authorized, only: [:create]
 
     def profile
       render json: { user: current_user }, status: :accepted
@@ -17,13 +17,7 @@ class Api::V1::UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        # render json: user.as_json(only: {:name, :email}, include: {purchases: {include: :item}, :orders})
-        #render json: @user, only: [:name, :email], include: [:orders]
-        #render json: @user.as_json(:include => {:orders => {:include => :purchases}})
-        #render :json => @user.to_json( :include => { :orders})
-        # render :json: @user include:{orders: {include:{purchases:}}}
 
-        #@object.to_json {:include => { :assocation_a => { :methods => :my_method }, :assocation_b => {} }}
         render json: @user, serializer: UserSerializer
     end
 
